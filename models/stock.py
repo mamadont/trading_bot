372ee244b18c4b5b455b0f1571e06a34a87f70c2
Mamadou Coulibaly <mamadoucoulibaly@Mamadous-MacBook-Pro.local>
2022-05-23 13:48:08 -0400

    first commit

---
+++ models/stock.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+class Stock:
+    def __init__(self, ticker_symbol, df):
+        self._ticker_symbol = ticker_symbol
+        self._rsi = 0
+        self._vwap = 0
+        self._ema = 0
+        self._macd = 0
+        self._price_action = df
+    
+    @property
+    def ticker_symbol(self):
+        return self._ticker_symbol
+
+    @ticker_symbol.setter
+    def rsi(self, ticker_symbol):
+        self._ticker_symbol = ticker_symbol
+
+    @property
+    def rsi(self):
+        return self._rsi
+
+    @rsi.setter
+    def rsi(self, rsi):
+        self._rsi = rsi
+
+    @property
+    def vwap(self):
+        return self._vwap
+        
+    @vwap.setter
+    def vwap(self, vwap):
+        self._vwap = vwap
+
+    @property
+    def ema(self):
+        return self._ema
+        
+    @ema.setter
+    def ema(self, ema):
+        self._ema = ema
+
+    @property
+    def macd(self):
+        return self._macd
+        
+    @macd.setter
+    def macd(self, macd):
+        self._macd = macd
+    
+    @property
+    def price_action(self):
+        return self._price_action
+    
+    @price_action.setter
+    def price_action(self, price_action):
+        self._price_action = price_action
---
+++ models/trading_bot.py
@@ -0,0 +1,54 @@
+import yfinance as yf
+from models.stock import Stock
+
+class TradingBot:
+    def __init__(self, tickers, captial):
+        self._tickers = tickers
+        self._watchlist = []
+        self._current_trades = []
+        self._capital = captial
+        
+        for ticker in tickers:
+            df = yf.download(ticker, period = "1d", interval = "5m")
+            self._watchlist.append(Stock(ticker, df))
+
+        print("Trading bot initialized...")
+    
+    def download_data(self):
+        for stock in self._watchlist:
+            df = yf.download(stock._ticker_symbol, period = "1d", interval = "5m")
+            stock._price_action = df
+
+    @property
+    def tickers(self):
+        return self.tickers
+    
+    @tickers.setter
+    def tickers(self, tickers):
+        self.tickers = tickers
+
+    @property
+    def watchlist(self):
+        return self._watchlist
+    
+    @watchlist.setter
+    def watchlist(self, watchlist):
+        self._watchlist = watchlist
+
+    @property
+    def current_trades(self):
+        return self._current_trades
+    
+    def add_trade(self, trade):
+        self._current_trades.append(trade)
+
+    @property
+    def captial(self):
+        return self.captial
+    
+    @captial.setter
+    def watchlist(self, captial):
+        self.captial = captial
+
+    
+
