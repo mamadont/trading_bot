10a28686e805aa7d3cf430d4d3309dbe28aa6da1
Mamadou Coulibaly <mamadoucoulibaly@Mamadous-MacBook-Pro.local>
2022-05-27 10:11:06 -0400

    implemented pandas_ta strategy

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from models.trading_bot import TradingBot
 
 tickers = ["MSFT", "AAPL", "NVDA", "AMD", "DAL"]
 trading_bot = TradingBot(tickers, 1000)
-trading_bot.calculate_rsi()
-print(trading_bot._watchlist[0]._rsi)
+trading_bot.run_strategy()
+ 
--- models/trading_bot.py
+++ models/trading_bot.py
@@ -1,5 +1,8 @@
 import yfinance as yf
 from models.stock import Stock
+import pandas_ta as ta
+import pandas as pd
+
 
 class TradingBot:
     def __init__(self, tickers, captial):
@@ -7,17 +10,13 @@ class TradingBot:
         self._watchlist = []
         self._current_trades = []
         self._capital = captial
-        
-        for ticker in tickers:
-            df = yf.download(ticker, period = "1d", interval = "5m")
-            self._watchlist.append(Stock(ticker, df))
 
         print("Trading bot initialized...")
 
     @property
     def tickers(self):
         return self.tickers
-    
+
     @tickers.setter
     def tickers(self, tickers):
         self.tickers = tickers
@@ -25,7 +24,7 @@ class TradingBot:
     @property
     def watchlist(self):
         return self._watchlist
-    
+
     @watchlist.setter
     def watchlist(self, watchlist):
         self._watchlist = watchlist
@@ -33,37 +32,40 @@ class TradingBot:
     @property
     def current_trades(self):
         return self._current_trades
-    
+
     def add_trade(self, trade):
         self._current_trades.append(trade)
 
     @property
     def captial(self):
         return self.captial
-    
+
     @captial.setter
     def watchlist(self, captial):
         self.captial = captial
 
     def download_data(self):
         for stock in self._watchlist:
-            df = yf.download(stock._ticker_symbol, period = "1d", interval = "5m")
+            df = yf.download(stock._ticker_symbol, period="1d", interval="5m")
             stock._price_action = df
 
-    def calculate_rsi(self, window=14, adjust=False):
-        for stock in self._watchlist:
-            delta = stock._price_action['Close'].diff(1).dropna()
-            loss = delta.copy()
-            gains = delta.copy()
+    def run_strategy(self):
+        Strategy = ta.Strategy(
+            name="My Strategy",
+            description="Uses MACD, RSI, VWAP, and EMA",
+            ta=[
+                {"kind": "ema", "length": 9},
+                {"kind": "vwap", "length": 21},
+                {"kind": "rsi"},
+                {"kind": "macd", "fast": 8, "slow": 21,
+                 "col_names": ("MACD", "MACD_H", "MACD_S")}
+            ]
+        )
 
-            gains[gains < 0] = 0
-            loss[loss > 0] = 0
+        df = pd.DataFrame()
 
-            gain_ewm = gains.ewm(com=window - 1, adjust=adjust).mean()
-            loss_ewm = abs(loss.ewm(com=window - 1, adjust=adjust).mean())
-
-            RS = gain_ewm / loss_ewm
-            RSI = 100 - 100 / (1 + RS)
-
-            stock._rsi = RSI.tail(1)[-1]
-
+        for stock in self._tickers:            
+            df = df.ta.ticker(stock, period="1d", interval="30m")
+            df.ta.strategy(Strategy)
+            
+        print('Data downloaded...')
