c03b86cc0a9910291cb2e85f5f8a41556e5324a0
Mamadou Coulibaly <mamadoucoulibaly@Mamadous-MacBook-Pro.local>
2022-06-01 11:02:43 -0400

    fixed nonype error

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 from models.trading_bot import TradingBot
-import yfinance as yf
-from models.stock import Stock
 
-tickers = ["SPY", "AAPL", "MSFT", "NVDA"]
+tickers = ["SPY", "QQQ"]
 trading_bot = TradingBot(tickers, 1000)
-
-df = yf.download("SPY", period="1d", interval="5m")
-stock = Stock("SPY", df)
-
 trading_bot.run_strategy()
  
--- models/trading_bot.py
+++ models/trading_bot.py
@@ -9,8 +9,9 @@ class TradingBot:
         self._capital = captial
 
         for ticker in tickers:
-            df = yf.download(ticker, period="1d", interval="5m")
-            self._watchlist.append(Stock(ticker, df))
+            df = yf.download(ticker, period="5d", interval= "5m")
+            stock = Stock(ticker, df)
+            self._watchlist.append(stock)
         
     @property
     def watchlist(self):
@@ -29,31 +30,37 @@ class TradingBot:
 
     @property
     def captial(self):
-        return self.captial
+        return self._captial
 
     @captial.setter
-    def watchlist(self, captial):
-        self.captial = captial
+    def capital(self, captial):
+        self._captial = captial
 
     def calc_rsi(self, stock: Stock):
         rsi = ta.rsi(stock._df["Close"])
         stock._rsi = rsi.tail()[-1]
+        print(f"RSI: {stock._rsi}")
      
     def calc_vwap(self, stock: Stock):
         vwap = ta.vwap(high= stock._df["High"], low= stock._df["Low"], close= stock._df["Close"], volume= stock._df["Volume"])
         stock._vwap = vwap.tail()[-1]
+        print(f"VWAP: {stock._vwap}")
 
     def calc_ema(self, stock: Stock):
         ema = ta.ema(stock._df["Close"], length= 5)
         stock._ema = ema.tail()[-1]
+        print(f"EMA: {stock._ema}")
     
     def calc_macd(self, stock: Stock):
         macd = ta.macd(stock._df["Close"])
         stock._macd = macd["MACD_12_26_9"].tail()[-1]
         stock._macd_slow = macd["MACDs_12_26_9"].tail()[-1]
+        print(f"MACD: {stock._macd}")
+        print(f"MACD SLOW: {stock._macd_slow}")
 
     def calc_technicals(self):
         for stock in self._watchlist:
+            print(f"{stock._ticker_symbol}")
             self.calc_macd(stock)
             self.calc_rsi(stock)
             self.calc_vwap(stock)
@@ -70,13 +77,13 @@ class TradingBot:
             else:
                 print(f"Not trading {stock._ticker_symbol}")
     
-    def isBullish(self, stock: Stock):
+    def isBullish(self, stock: Stock): # need to implement crossovers
         if (stock._macd > stock._macd_slow):
             if (stock._rsi > 50):
                 if (stock._ema > stock._vwap):
                     return True
 
-    def isBearish(self, stock: Stock):
+    def isBearish(self, stock: Stock): # need to implement crossovers
         if (stock._macd < stock._macd_slow):
             if (stock._rsi < 50):
                 if (stock._ema < stock._vwap):
