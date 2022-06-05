d5355e78444226b4743642131ea7f08ed145f9ab
Mamadou Coulibaly <mamadoucoulibaly@Mamadous-MacBook-Pro.local>
2022-06-05 18:18:38 -0400

    adding some entry logic along with buying integration

--- models/trade.py
+++ models/trade.py
@@ -1,2 +1,58 @@
+from importlib.metadata import entry_points
+from time import time
+from urllib3 import Retry
+
+
 class Trade:
-    pass
+    def __init__(self, ticker_symbol, type, entry_price, time_of_entry):
+        self._ticker_symbol = ticker_symbol
+        self._type = type
+        self._entry_price = entry_price
+        self._time_of_entry = time_of_entry
+        self._exit_price = 0
+        self._profit = 0
+
+    @property
+    def ticker_symbol(self):
+        return self.ticker_symbol
+    
+    @ticker_symbol.setter
+    def ticker_symbol(self, ticker_symbol):
+        self.ticker_symbol = ticker_symbol
+    
+    @property
+    def type(self):
+        return self.type
+    
+    @type.setter
+    def tpye(self, type):
+        self.type = type
+
+    @property
+    def entry_price(self):
+        return self.entry_price
+    
+    @entry_price.setter
+    def entry_price(self, entry_price):
+        self.entry_price = entry_price
+
+    @property
+    def time_of_entry(self):
+        return self.time_of_entry
+    
+    @time_of_entry.setter
+    def property(self, time_of_entry):
+        self.time_of_entry = time_of_entry
+
+    @property
+    def exit_price(self):
+        return self.exit_price
+    
+    @exit_price.setter
+    def property(self, exit_price):
+        self.exit_price = exit_price
+
+
+    
+
+    
--- models/trading_bot.py
+++ models/trading_bot.py
@@ -1,9 +1,11 @@
+from turtle import position
 import yfinance as yf
 from models.stock import Stock
 from models.strategy import Strategy
 import alpaca_trade_api as tradeapi
 from keys import api_key, secret_key
-
+from models.trade import Trade
+import time
 
 class TradingBot:
     def __init__(self, tickers, captial):
@@ -13,6 +15,8 @@ class TradingBot:
         self.strategy = Strategy()
         self.api_key = api_key
         self.secret_key = secret_key
+        self.api = tradeapi.REST(key_id=self.api_key, secret_key=self.secret_key, 
+                    base_url="https://paper-api.alpaca.markets", api_version='v2')
 
         for ticker in tickers:
             df = yf.download(ticker, period="5d", interval= "5m")
@@ -48,26 +52,37 @@ class TradingBot:
         for stock in self._watchlist:
             if self.strategy.isBullish(stock):
                 print(f"Bullish trade for: {stock._ticker_symbol}")
-                # TODO: sell any puts 
-                self.enter_calls(stock._ticker_symbol)
+
+                if self.already_in_trade(stock._ticker_symbol):
+                    print("But you are already in a call for this ticker")
+                else:
+                    self.enter_calls(stock._ticker_symbol)
+                
             elif self.strategy.isBearish(stock):
                 print(f"Bearish trade for: {stock._ticker_symbol}")
-                # TODO: sell any calls
-                self.enter_puts(stock._ticker_symbol)
+
+                if self.already_in_trade(stock._ticker_symbol):
+                    print("But you are already in a put for this ticker")
+                else:
+                    self.enter_puts(stock._ticker_symbol)
             else:
                 print(f"Not trading {stock._ticker_symbol}")
 
     def enter_calls(self, stock: Stock):
-        BASE_URL = "https://paper-api.alpaca.markets"
-        api = tradeapi.REST(key_id=self.api_key, secret_key=self.secret_key, 
-                    base_url=BASE_URL, api_version='v2')
-        api.submit_order(symbol=stock.ticker_symbol, qty=1, side='buy', type='market', time_in_force='day')
+        self.api.submit_order(symbol=stock.ticker_symbol, qty=1, side='buy', type='market', time_in_force='day')
+        position = tradeapi.get_position(stock._ticker_symbol)
+        self._current_trades.append(Trade(position.symbol, "call", position.current_value, time.localtime()))
 
     def enter_puts(self, stock: Stock):
-        BASE_URL = "https://paper-api.alpaca.markets"
-        api = tradeapi.REST(key_id=self.api_key, secret_key=self.secret_key, 
-                    base_url=BASE_URL, api_version='v2')
-        api.submit_order(symbol=stock.ticker_symbol, qty=1, side='sell', type='market', time_in_force='day')
+        self.api.submit_order(symbol=stock.ticker_symbol, qty=1, side='sell', type='market', time_in_force='day')
+        position = tradeapi.get_position(stock._ticker_symbol)
+        self._current_trades.append(Trade(position.symbol, "call", position.current_value, time.localtime()))
+
+    def already_in_trade(self, ticker_symbol):
+        for trade in self.current_trades:
+            if trade.ticker_symbol == ticker_symbol:
+                return True
+        return False
 
     def exit_position():
         pass
